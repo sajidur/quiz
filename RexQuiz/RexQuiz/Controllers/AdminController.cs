017cdf241be389911fac9ca7b6271773cd9a55de
Md. Zahid Hasan <zahid.cse07@gmail.com>
2018-12-01 21:37:19 +0600

    add login and registration

---
+++ RexQuiz/RexQuiz/Controllers/AdminController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RexQuiz.Controllers
+{
+    public class AdminController : Controller
+    {
+       
+        
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+
+      
+    }
+}
