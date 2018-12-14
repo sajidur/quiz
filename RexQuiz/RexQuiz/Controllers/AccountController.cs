17719a83318320632ac865d76b70cf0bf4479d1a
Md. Zahid Hasan <zahid.cse07@gmail.com>
2018-12-14 23:01:22 +0600

    login and register api call

--- RexQuiz/RexQuiz/Controllers/AccountController.cs
+++ RexQuiz/RexQuiz/Controllers/AccountController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using RexQuiz.Models;
 
 namespace RexQuiz.Controllers
 {
@@ -13,8 +15,54 @@ namespace RexQuiz.Controllers
             return View();
         }
 
+        [HttpPost]
+        public IActionResult Index(string userName,string password)
+        {
+           // userName = "z@gmail.com";password = "Zahid1234*";
+            if (userName=="admin"&& password=="admin")
+            {
+                return RedirectToAction("Dashboard", "Admin");
+
+            }
+            using (var client = new HttpClient())
+            {
+                string url = "userName="+ userName+"&password="+password;
+                client.BaseAddress = new Uri("http://sajidur-001-site3.itempurl.com/api/Account/Login?" + url);
+
+                var postTask = client.PostAsync("",null);
+                postTask.Wait();
+                var result = postTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index", "Student");
+                }
+            }
+            return View();
+        }
+
         public IActionResult Register()
         {
+            ViewData["a"] = "zzzz";
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(Account account)
+        {
+            using (var client =new HttpClient())
+            {
+                client.BaseAddress=new Uri("http://sajidur-001-site3.itempurl.com/api/Account/Register");
+
+                var postTask = client.PostAsJsonAsync<Account>("account", account);
+                postTask.Wait();
+                var result = postTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    return Redirect("Index");
+                }
+            }
+            ModelState.AddModelError(String.Empty, "Server Error.");
+            //ViewData["a"] = "jabjf";
             return View();
         }
     }
--- RexQuiz/RexQuiz/Models/Account.cs
+++ RexQuiz/RexQuiz/Models/Account.cs
@@ -7,8 +7,9 @@ namespace RexQuiz.Models
 {
     public class Account
     {
-         public string Email { get; set; }
-         public string Password { get; set; }
-         public string ConfirmPassword { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
     }
 }
