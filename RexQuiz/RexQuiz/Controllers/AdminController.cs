b46695d22a88e4a1325bab2dc6cf1dc1a4902a8e
Md. Zahid Hasan <zahid.cse07@gmail.com>
2019-01-14 01:40:41 +0600

    quiz controller updated

--- RexQuiz/RexQuiz/Controllers/AdminController.cs
+++ RexQuiz/RexQuiz/Controllers/AdminController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using RexQuiz.Models;
 
 namespace RexQuiz.Controllers
 {
@@ -24,9 +26,38 @@ namespace RexQuiz.Controllers
         {
             return View();
         }
+
         public IActionResult Quiz()
         {
-            return View();
+
+            IEnumerable<Quiz> students = null;
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:64189/api/values");
+                //HTTP GET
+                var responseTask = client.GetAsync("");
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<IList<Quiz>>();
+                    readTask.Wait();
+
+                    students = readTask.Result;
+                }
+                else //web api sent error response 
+                {
+                    //log response status here..
+
+                    students = Enumerable.Empty<Quiz>();
+
+                    ModelState.AddModelError(string.Empty, "Server error. Please contact administrator.");
+                }
+
+                return View();
+            }
         }
 
         public IActionResult Invite()
---
+++ RexQuiz/RexQuiz/Models/Quiz.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RexQuiz.Models
+{
+    public class Quiz
+    {
+        public int QuizId { get; set; }
+        public string QuizTitle { get; set; }
+        public string QuizType { get; set; }
+        public string QuizLevel { get; set; }
+        public int NoOfQuestion { get; set; }
+    }
+}
